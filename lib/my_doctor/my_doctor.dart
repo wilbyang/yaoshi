4198f2107a034eb3d82c924840d551b598921fdc
boyang <yang.wilby@gmail.com>
2019-04-10 16:47:11 +0200

    1. ui layout. 2. remove the firestore dependency

--- lib/my_doctor/my_doctor.dart
+++ lib/my_doctor/my_doctor.dart
@@ -21,6 +21,72 @@ class _MyDoctorPageState extends State<MyDoctorPage> {
       _selectedIndex = index;
     });
   }
+
+  Widget _buildTitleSection(String title, String name) {
+
+    const textBottomEdgeInsets = const EdgeInsets.only(bottom: 8);
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.only(right: 32, top: 8),
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage('http://i.imgur.com/QSev0hg.jpg'),
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: textBottomEdgeInsets,
+                  child: Text(
+                    '$name',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: textBottomEdgeInsets,
+                  child: Text(
+                    '$title',
+                  ),
+                ),
+                Container(
+                  padding: textBottomEdgeInsets,
+                  child: Text(
+                    '$title',
+                  ),
+                ),
+                Text(
+                  '肾内科, 主任医师',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Icon(Icons.star, color: Colors.red[500]),
+              Text('41'),
+            ],
+          ),
+        ],
+      ),
+    );
+    return titleSection;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,16 +107,11 @@ class _MyDoctorPageState extends State<MyDoctorPage> {
           onTap: _onItemTapped
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: ListView(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_selectedIndex',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            _buildTitleSection("武汉市中南湘雅同济医院", "张壮"),
+            _buildTitleSection("北京协和医院", "刘洪波"),
+            _buildTitleSection("解放军总医院", "常梦然"),
           ],
         ),
       ),
