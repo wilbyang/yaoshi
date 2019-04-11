ea9d929ecaf919560eee69869a9889c690abf5fa
boyang <yang.wilby@gmail.com>
2019-04-11 19:42:21 +0200

    connect to firestore

--- lib/my_doctor/my_doctor.dart
+++ lib/my_doctor/my_doctor.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 class MyDoctorPage extends StatefulWidget {
   MyDoctorPage({Key key, this.title}) : super(key: key);
 
@@ -22,10 +23,62 @@ class _MyDoctorPageState extends State<MyDoctorPage> {
     });
   }
 
-  Widget _buildTitleSection(String title, String name) {
 
-    const textBottomEdgeInsets = const EdgeInsets.only(bottom: 8);
-    Widget titleSection = Container(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(widget.title),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('我的医生')),
+            BottomNavigationBarItem(icon: Icon(Icons.business), title: Text('iCare')),
+            BottomNavigationBarItem(icon: Icon(Icons.school), title: Text('严选')),
+            BottomNavigationBarItem(icon: Icon(Icons.person), title: Text('我')),
+          ],
+          currentIndex: _selectedIndex,
+          unselectedItemColor: Colors.black38,
+          selectedItemColor: Colors.blueAccent,
+          onTap: _onItemTapped
+      ),
+      body: Center(
+        child: MyDoctorList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+class Doctor {
+  final String name;
+  final String title;
+  final String major;
+  final String resume;
+  final String org;
+  final String sector;
+  Doctor({@required this.name, @required this.org, this.major, this.title, this.sector, this.resume});
+  factory Doctor.fromFireStoreDoc(DocumentSnapshot doc) {
+    return Doctor(name: doc['name'], title: doc["title"], org: doc['org'], major: doc["major"], resume: doc["resume"], sector: doc["sector"]);
+  }
+}
+class DoctorTileWidget extends StatelessWidget {
+
+  final Doctor doctor;
+  static const EdgeInsets textBottomEdgeInsets = const EdgeInsets.only(bottom: 8);
+  const DoctorTileWidget(this.doctor, {
+    Key key,
+  }) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
       padding: const EdgeInsets.fromLTRB(32, 8, 32, 12),
       decoration: BoxDecoration(border: Border(
         bottom: BorderSide(color: Colors.black12)
@@ -53,7 +106,7 @@ class _MyDoctorPageState extends State<MyDoctorPage> {
                     Container(
                       padding: textBottomEdgeInsets,
                       child: Text(
-                        '$name',
+                        '${doctor.name}',
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                         ),
@@ -67,11 +120,11 @@ class _MyDoctorPageState extends State<MyDoctorPage> {
                 Container(
                   padding: textBottomEdgeInsets,
                   child: Text(
-                    '$title',
+                    '${doctor.org}',
                   ),
                 ),
                 Text(
-                  '肾内科, 主任医师',
+                  '${doctor.sector}, ${doctor.title}',
                   style: TextStyle(
                     color: Colors.grey[500],
                   ),
@@ -93,44 +146,6 @@ class _MyDoctorPageState extends State<MyDoctorPage> {
         ],
       ),
     );
-    return titleSection;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(widget.title),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('我的医生')),
-            BottomNavigationBarItem(icon: Icon(Icons.business), title: Text('iCare')),
-            BottomNavigationBarItem(icon: Icon(Icons.school), title: Text('严选')),
-            BottomNavigationBarItem(icon: Icon(Icons.person), title: Text('我')),
-          ],
-          currentIndex: _selectedIndex,
-          unselectedItemColor: Colors.black38,
-          selectedItemColor: Colors.blueAccent,
-          onTap: _onItemTapped
-      ),
-      body: Center(
-        child: ListView(
-          children: <Widget>[
-            _buildTitleSection("武汉市中南湘雅同济医院", "张壮"),
-            _buildTitleSection("北京协和医院", "刘洪波"),
-            _buildTitleSection("解放军总医院", "常梦然"),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
   }
 }
 
@@ -149,6 +164,28 @@ class HotIndexWidget extends StatelessWidget {
       children: <Widget>[
         Text("$title", style: TextStyle(fontSize: 10.0),)
       ]..addAll(list),
+    );
+  }
+}
+
+class MyDoctorList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('doctors').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError)
+          return new Text('Error: ${snapshot.error}');
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting: return new Text('Loading...');
+          default:
+            return new ListView(
+              children: snapshot.data.documents.map((DocumentSnapshot document) {
+                return DoctorTileWidget(Doctor.fromFireStoreDoc(document));
+              }).toList(),
+            );
+        }
+      },
     );
   }
 }
