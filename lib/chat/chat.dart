592e31f27c29c370e780f9ba3fb6297d3da6288d
boyang <yang.wilby@gmail.com>
2019-04-18 11:04:23 +0200

    chatpage is ready

--- lib/chat/chat.dart
+++ lib/chat/chat.dart
@@ -1,14 +1,159 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ChatWidget extends StatelessWidget {
+class ChatPage extends StatefulWidget {
+  @override //new
+  State createState() => ChatPageState();
+}
+
+class ChatPageState extends State<ChatPage> with TickerProviderStateMixin {
+  final TextEditingController _textController = TextEditingController();
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  bool _isComposing = false;
   @override
   Widget build(BuildContext context) {
-
-    return Column(
-      children: <Widget>[
-        Text("嘿嘿，这里是聊天界面"),
-      ],
+    final decoration = BoxDecoration(
+      border: Border(
+        top: BorderSide(color: Colors.grey[200]),
+      ),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Friendlychat"),
+        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Flexible(
+              child: ListView.builder(
+                padding: EdgeInsets.all(8.0),
+                reverse: true,
+                itemBuilder: (_, int index) => _messages[index],
+                itemCount: _messages.length,
+              ),
+            ),
+            Divider(height: 1.0),
+            Container(
+              decoration: BoxDecoration(color: Theme.of(context).cardColor),
+              child: _buildTextComposer(),
+            ),
+          ],
+        ),
+        decoration: Theme.of(context).platform == TargetPlatform.iOS
+            ? decoration
+            : null,
+      ),
     );
   }
 
+  @override
+  void dispose() {
+    for (ChatMessage message in _messages) //new
+      message.animationController.dispose(); //new
+    super.dispose();
+  }
+
+  Widget _buildTextComposer() {
+    Widget widget;
+    if (Theme.of(context).platform == TargetPlatform.iOS) {
+      widget = CupertinoButton(
+        child: Text("Send"), //new
+        onPressed: () {
+          if (_isComposing) _handleSubmitted(_textController.text);
+        },
+      );
+    } else {
+      widget = IconButton(
+        icon: Icon(Icons.send),
+        onPressed: () {
+          if (_isComposing) _handleSubmitted(_textController.text);
+        },
+      );
+    }
+
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        children: <Widget>[
+          Flexible(
+            child: TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration: InputDecoration.collapsed(hintText: "Send a message"),
+              onChanged: (text) {
+                setState(() {
+                  _isComposing = text.length > 0;
+                });
+              },
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 4.0),
+            child: widget,
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    setState(() {
+      _isComposing = false;
+    });
+    ChatMessage message = ChatMessage(
+      text: text,
+      animationController: AnimationController(
+        vsync: this,
+        duration: Duration(
+          milliseconds: 700,
+        ),
+      ),
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+}
+
+const String _name = "Your Name";
+
+class ChatMessage extends StatelessWidget {
+  final String text;
+  final AnimationController animationController;
+  ChatMessage({this.text, this.animationController});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizeTransition(
+      sizeFactor: CurvedAnimation(
+        parent: animationController,
+        curve: Curves.easeOut,
+      ),
+      axisAlignment: 0.0,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(right: 16.0),
+              child: CircleAvatar(child: Text(_name[0])),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(_name, style: Theme.of(context).textTheme.subhead),
+                Container(
+                  margin: EdgeInsets.only(top: 5.0),
+                  child: Text(text),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:yaoshi/i_care/i_care.dart';
 import 'package:yaoshi/my_doctor/my_doctor.dart';
@@ -5,25 +6,25 @@ import 'package:yaoshi/profile/profile.dart';
 import 'package:yaoshi/store/store.dart';
 
 void main() => runApp(MyApp());
+final ThemeData kIOSTheme = new ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
 
+final ThemeData kDefaultTheme = new ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      theme: defaultTargetPlatform == TargetPlatform.iOS         //new
+          ? kIOSTheme                                              //new
+          : kDefaultTheme,
       home: HomePage(title: '我的医生'),
     );
   }
